2c8052763e0eb6498a29ef6fd20f63c629b13dad
guilleeh <guilleeh@uci.edu>
2020-02-18 11:29:33 -0800

    restructured ios app

---
+++ HealthieApp/HealthieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthieApp
+//
+//  Created by Guillermo Hernandez on 2/18/20.
+//  Copyright © 2020 Guillermo Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
