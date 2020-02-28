70be3ba50e5f24e899f8252f97d94287bbbf64a5
Salvador Villalon <salvav1@uci.edu>
2020-02-27 20:25:04 -0800

    added code to call recipe api

--- HealthieBackend/app.py
+++ HealthieBackend/app.py
@@ -12,8 +12,8 @@ from database import db
 
 app = Flask(__name__)
 app.config.from_envvar('ENV_FILE_LOCATION')
-app.config["MONGO_DBNAME"] = "recipe_database"
-app.config["MONGO_URI"] = "mongodb://salvillalon45_notread:45%$HiFriend@ds263028.mlab.com:63028/recipe_database"
+# app.config["MONGO_DBNAME"] = "recipe_database"
+# app.config["MONGO_URI"] = "mongodb://salvillalon45:HolaAmigo45%$@ds263028.mlab.com:63028/recipe_database"
 
 api = Api(app, errors=errors)
 bcrypt = Bcrypt(app)
@@ -22,10 +22,10 @@ jwt = JWTManager(app)
 initialize_db(app)
 initialize_routes(api)
 
-recipe_collection = db.initalize_mongodb_client()
-recipe_list = edamam.recipe_search()
+recipe_collection = db.initialize_mongodb_client()
+# recipe_list = edamam.recipe_search()
 # edmama.insert_to_collection(recipe_list, recipe_collection)
-edamam.view_content_collection(recipe_collection)
+db.view_content_collection(recipe_collection)
 
 if __name__ == '__main__':
     app.run(debug=True, load_dotenv=True)
--- HealthieBackend/database/db.py
+++ HealthieBackend/database/db.py
@@ -3,6 +3,16 @@ import pymongo
 from flask_mongoengine import MongoEngine
 
 db = MongoEngine()
+DB_NAME = "recipe_database"
+DB_HOST = "ds263028.mlab.com"
+DB_PORT = 63028
+DB_USER = "salvillalon45"
+DB_PASS = "HolaAmigo45%$"
+
+
+# connection = MongoClient(DB_HOST, DB_PORT)
+# db = connection[DB_NAME]
+# db.authenticate(DB_USER, DB_PASS)
 
 
 def initialize_db(app):
@@ -16,16 +26,18 @@ def initialize_mongodb_client():
     print("Creating client... ")
     # Assigns localhost:27017 to be the host for the Mongodb client
     # local_db_client = pymongo.MongoClient("mongodb://localhost:27017/")
-    mlab_db_client = pymongo.MongoClient("mongodb://salvillalon45_notread:45%$HiFriend@ds263028.mlab.com:63028/recipe_database")
+    # mlab_db_client = pymongo.MongoClient("mongodb://salvillalon45:HolaAmigo45%$@ds263028.mlab.com:63028/recipe_database")
+    mlab_db_client = pymongo.MongoClient(DB_HOST, DB_PORT)
 
     # creates search database
     print("Creating database")
-    db = mlab_db_client.recipe_db
+    recipe_db = mlab_db_client.recipe_db
+    recipe_db.authenticate(DB_USER, DB_PASS)
 
     # The database that will be used to search
     # It was made global because the search function is seperate see search_it()
     global recipe_collection
-    # recipe_collection = db.recipe_collection
+    recipe_collection = recipe_db.recipe_collection
 
     print("Creating collection::", recipe_collection)
     return recipe_collection
@@ -43,6 +55,7 @@ def view_content_collection(recipe_collection):
     print("Inside view_content_collection()")
 
     cursor = recipe_collection
+    print("What is recipe_collection:: ", (recipe_collection is None))
 
     for document in cursor.find():
         print(document)
