3f850218e21f5ce54d6b8cc460d94878af21bfb7
Salvador Villalon <salvav1@uci.edu>
2020-03-02 17:09:07 -0800

    made changes to my last pull request

--- HealthieBackend/database/models.py
+++ HealthieBackend/database/models.py
@@ -18,9 +18,8 @@ class User(db.Document):
     # allergies (cautions)
 
 
-
     def hash_password(self):
         self.password = generate_password_hash(self.password).decode('utf8')
-    
+
     def check_password(self, password):
         return check_password_hash(self.password, password)
