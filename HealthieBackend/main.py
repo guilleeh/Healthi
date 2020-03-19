24a3c7b422d0255782d71a63dc06124e32b1963b
jecorona97 <jecorona97@gmail.com>
2020-03-18 19:27:22 -0700

    refactored search.py

--- HealthieBackend/main.py
+++ HealthieBackend/main.py
@@ -4,16 +4,17 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from database.db import initialize_db
+from resources.apis.search import initialize_es
 from flask_restful import Api
 from resources.apis.routes import initialize_routes
 from resources.apis.errors import errors
-from elasticsearch import Elasticsearch
 
 
 app = Flask(__name__)
-app.config.from_json('config.json')
-app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
-        if app.config['ELASTICSEARCH_URL'] else None
+
+# app.config.from_json('config.json')
+app.config.from_json('local_config.json')
+
 
 api = Api(app, errors=errors)
 bcrypt = Bcrypt(app)
@@ -21,6 +22,7 @@ jwt = JWTManager(app)
 
 initialize_db(app)
 initialize_routes(api)
+initialize_es(app)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True, load_dotenv=True)
